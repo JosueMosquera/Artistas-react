378d9a8bec0d4c8b4d145f68fa6045362ae7490d
JosueMosquera <82737793+JosueMosquera@users.noreply.github.com>
2021-10-13 09:41:12 -0500

    artistas-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import CardsGrid from "./containers/CardsGrid";
+import "./App.css";
+import logo from "./img/logops4.png";
 function App() {
+  const [cards, SetCards] = useState([]);
+  const [search, setSearch] = useState("");
+  const ConectarApi = async () => {
+    const resp = await fetch(
+      "https://rest-artistas-indieec.herokuapp.com/api/users"
+    );
+    const data = await resp.json();
+    SetCards(data.usuarios);
+  };
+  useEffect(() => ConectarApi(), []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="navcontainer">
+        <div id="nav">
+          <nav
+            className="navbar navbar-light"
+            style={{ backgroundColor: "#243b44" }}
+          >
+            <div className="container-fluid">
+              <a
+                className="navbar-brand"
+                href="https://indieec.github.io/"
+                style={{ marginLeft: "15px", color: "white"}}
+              >
+                <img
+                  src={logo}
+                  style={{ height: "50px",width:'80px',imageResolution: 100,marginRight:"15px" }}
+                  className="img-fluid"
+                  alt=""
+                />
+                Home
+              </a>
+
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Buscar"
+                  aria-label="Search"
+                  onChange={(e) => setSearch(e.target.value.toLowerCase())}
+                />
+                <button className="btn btn-success" type="submit" onClick={(e) => e.preventDefault()}>
+                  Buscar
+                </button>
+              </form>
+            </div>
+          </nav>
+        </div>
+        </div>
       </header>
+      <CardsGrid artistas={cards} search={search} />
     </div>
   );
 }
---
+++ src/containers/CardsGrid.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from '../components/Card'
+import styles from "../styles/Cards.module.css"
+function CardsGrid({artistas,search}) {
+    const filteredCards =artistas.filter((artista)=>artista.nombre.toLowerCase().includes(search))
+    return (
+        <div className="container">
+                <div className="row">
+                    <div className="col mb-3 mt-4  text-uppercase" id="subtitle">
+                        <h2 className={styles.title}>Artistas</h2>
+                    </div>
+                    <center>
+                    <div className="container">
+                        <div className="row">
+                        {filteredCards.map(artista=><Card artista={artista} key={artista.uid} />)}
+                        </div>
+                    </div>
+                    </center>
+                </div>
+            </div>
+    )
+}
+
+export default CardsGrid
